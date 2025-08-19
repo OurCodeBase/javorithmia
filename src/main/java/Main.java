a382c59e57ec2ee597c6bde159bdc17a557cfdda
hαrsh vαirαgi <100824266+OurCodeBase@users.noreply.github.com>
2025-08-19 05:59:30 +0000

    Basic IO and datatypes are explored!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,16 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    char c = 'a';
+    int number = 10;
+    boolean bool = true;
+    String hello = "Hello World";
+    System.out.println(hello);
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter your name: ");
+    String name = scanner.nextLine();
+    System.out.println("Hello, " + name + "!");
+  }
 }
